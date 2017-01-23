664dae015f305083b7339fac3d5ca721d097f36b
aixleaix <underteh@outlook.com>
2017-01-23 12:58:01 +0200

    J4_PropertiesAndChainableMethods.app.js

--- J4_PropertiesAndChainableMethods/Finished/app.js
+++ J4_PropertiesAndChainableMethods/Finished/app.js
@@ -1,4 +1,5 @@
 //teeb objekti nimega g milleks on globaalne objekt G$ millele sisestatakse ('John' ja 'Doe').
 var g = G$('John', 'Doe');
-//läbi konsooli on prinditud objekt g.
+//läbi konsooli on prinditud objekti g funktsioon greet(vaikimisi seadetes), setLang ja seejärel
+// uues keeles greet funktsioon
 g.greet().setLang('es').greet(true);
