3fcb83a7fc3a72a222bcc0351fb856eb787b5e3d
aixleaix <underteh@outlook.com>
2017-01-23 11:16:59 +0200

    J2_StructuringSafeCode_.Greetr.js

---
+++ J2_StructuringSafeCode/Finished/Greetr.js
@@ -0,0 +1,6 @@
+//Teeb uue objeki, funktsioon mille sisenditeks on global ja $
+(function(global, $) {
+//   funktsiooni struktuur on tühi
+    
+//peale funktsiooni tegemist, on kohe funktsioonile sisestatud window ja JQuery.
+}(window, jQuery));
