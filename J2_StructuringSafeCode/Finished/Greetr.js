be72a94691b431c6152335277ea69dae4e9f04cf
aixleaix <underteh@outlook.com>
2017-01-25 18:19:52 +0200

    J2 greetr.js

--- J2_StructuringSafeCode/Finished/Greetr.js
+++ J2_StructuringSafeCode/Finished/Greetr.js
@@ -1,4 +1,4 @@
 //turvaline/privaatne objekt, mille sisenditeks on globaalne väärtus ja jquery väärtus
 (function(global, $) {
 
-}(window, jQuery)); //funkstioonile sisestatakse globaalne väärtus window ja jquery väärtus
+}(window, jQuery)); //funkstioonile sisestatakse globaalne väärtus window ja jquery väärtus.
