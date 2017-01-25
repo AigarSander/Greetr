daff6159961640baa4c388cba2f0d6979a856a68
aixleaix <underteh@outlook.com>
2017-01-25 18:20:18 +0200

    J3 app.js

--- J3_OurObjectAndPrototype/Finished/app.js
+++ J3_OurObjectAndPrototype/Finished/app.js
@@ -1,4 +1,4 @@
 //teeb objekti nimega g milleks on globaalne objekt G$ millele sisestatakse ('John' ja 'Doe').
 var g = G$('John', 'Doe');
-//läbi konsooli on prinditud objekt g.
+//läbi konsooli on prinditud objekt g
 console.log(g);
